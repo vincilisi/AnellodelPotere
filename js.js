3396049d684bc0b580b21f10005e760acab390fe
vincilisi <96939594+vincilisi@users.noreply.github.com>
2024-07-30 16:00:24 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+let array_1 = [
+    ['un', 'per', 'incatenarli.'],
+    ['Anello', 'trovarli,'],
+    ['ghermirli', 'e'],
+    ['gondor', 'mark'],
+  ];
+  
+  let array_2 = [
+    [['trovarli,']],
+    ['tu,', 'sciocchi'],
+    ['tu,', 'sciocchi', ['padron', 'Sauron']],
+    ['nel', ['fuggite', 'gandalf']],
+    [['domarli,', 'passare'], 'buio']
+  ];
+
+"Un Anello per domarli, un Anello per trovarli, un Anello per ghermirli e nel buio incatenarli."
+
+var frase = array_1[0][0]+' ' +array_1[1][0]+' '+array_1[0][1]+' '+array_2[4][0][0]+' '+array_1[0][0]+' '+array_1[1][0]+' ' +array_1[1][0]+' '+array_1[0][1]+' '+array_1[1][1]+' '+array_1[0][0]+' ' +array_1[1][0]+' '+array_1[0][1]+' '+array_1[2][0]+' '+array_1[2][1]+' '+array_2[3][0]+' '+array_2[4][1]+' '+array_1[0][2];
+console.log(frase);
